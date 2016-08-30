acff81d97ff025f0704645c8a3090ae424fc16cf
yanghengzhan <yanghengzhan@sina.com>
2016-08-30 17:51:18 +0800

    Initial Commit

---
+++ YHGuevaraMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHGuevaraMovie
+//
+//  Created by yanghengzhan on 16/8/30.
+//  Copyright © 2016年 yanghengzhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
